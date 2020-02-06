6efc2b54c823323aaed181c0fd0dab25f75aec2b
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-02-06 10:52:20 +0100

    Struttura base

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+// Titolo
+// Titolo Originale
+// Lingua
+// Voto
+
+$(document).ready(function(){
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/search/movie',
+    method: 'GET',
+    data: {
+      api_key: '1fedcc210c1f6d12a58971ab67657552',
+      query: 'ritorno al futuro'
+    },
+    success: function(data){
+
+    },
+    error: function(request, state, errors){
+
+    }
+  });
+});
