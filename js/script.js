43343268f50a1c56a1734b3890cea67cb10cc73d
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-02-07 11:01:20 +0100

    Aggiunta funzione per conversione voto da 10 a 5 e stelle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
 // Titolo "title"
 // Titolo Originale "original_title"
 // Lingua "original_language"
 // Voto "vote_average"
+
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
+// permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+// lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze
+// piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della
+// nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
+// nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca
+// dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando
+// attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di
+// risposta diversi, simili ma non sempre identici)
 
 $(document).ready(function(){
   $(document).on('click', '.search-film', function(){
@@ -13,16 +24,36 @@ $(document).ready(function(){
       method: 'GET',
       data: {
         api_key: '1fedcc210c1f6d12a58971ab67657552',
-        query: inputFilm
+        query: inputFilm,
+        language: 'it-IT'
       },
       success: function(data){
-        findFilm(data);
+        if (data.total_results > 0){
+          clearResults();
+          findFilm(data);
+          clearInput();
+        } else {
+          clearResults();
+          noResultsMsg();
+          clearInput();
+        }
       },
       error: function(request, state, errors){
-
+        alert("C'è stato un problema " + errors);
       }
     });
   });
+
+  function noResultsMsg(){
+    var source = $("#no-results-template").html();
+    var template = Handlebars.compile(source);
+    var inputVal = $('.film-input').val();
+    var msg = {
+      message: 'Nessun risultato trovato per: ' + "'" + inputVal + "'"
+    };
+    var html = template(msg);
+    $('.filmsList').append(html);
+  }
 
 
   function findFilm(filmObj){
@@ -36,6 +67,41 @@ $(document).ready(function(){
       var html = template(singleFilm);
       $('.filmsList').append(html);
     }
+    voteFrom10To5();
+  }
+
+  function clearResults(){
+    $('.filmsList .film-template').hide();
+  }
+
+  function clearInput(){
+    $('.film-input').val('');
+  }
+
+  function voteFrom10To5(){
+    $('.vote').each(function(){
+      var singleVote = $(this).text();
+      console.log('voto originale ' + singleVote);
+      var singleVoteOn5 = Math.ceil(singleVote / 2);
+      console.log('voto su 5: ' + singleVoteOn5);
+      $(this).html(fromVoteToStars(singleVoteOn5));
+    });
+  }
+
+  function fromVoteToStars(vote){
+    var stars;
+    if (vote == 1){
+      stars = '<i class="fas fa-star"></i>';
+    } else if (vote == 2){
+      stars = '<i class="fas fa-star"></i> <i class="fas fa-star"></i>';
+    } else if (vote == 3){
+      stars = '<i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i>';
+    } else if (vote == 4){
+      stars = '<i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i>';
+    } else if (vote == 5){
+      stars = '<i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i>';
+    }
+    return stars;
   }
 
 });
